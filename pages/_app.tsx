9304d8a51651d8dbc9005785bb4d3f64a62b6f6a
Ernest Irabor <irabeny89@gmail.com>
2022-06-25 13:59:28 +0100

    Fix Google Tag Manager Script typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `(function (w, d, s, l, i) {
-            w[l] || [];
+            w[l] = w[l] || [];
             w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
             var f = d.getElementsByTagName(s)[0],
               j = d.createElement(s),
